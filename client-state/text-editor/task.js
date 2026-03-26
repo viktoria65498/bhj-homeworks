c28d508a7add267d8f505de638e0bf647fd52093
vkkk_a <grudinovaviktoria@gmail.com>
2026-03-26 20:46:24 +0300

    222

--- client-state/text-editor/task.js
+++ client-state/text-editor/task.js
@@ -1,10 +1,17 @@
 const editor = document.getElementById('editor')
 
-window.addEventListener('DOMContentLoaded', function() {
+window.addEventListener('DOMContentLoaded', () => {
     const saved = localStorage.getItem('editor')
     editor.value = saved
 })
 
-editor.addEventListener('input', function() {
+editor.addEventListener('input', () => {
     localStorage.setItem('editor', editor.value)
+})
+
+const button = document.getElementById('button')
+
+button.addEventListener('click', () => {
+    localStorage.removeItem('editor')
+    editor.value = ''
 })
